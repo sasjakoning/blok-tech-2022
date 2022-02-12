be1f513af5cb32c4c8b9df2fd856dd13ebbd7c49
sasjakoning <sasjakoning@hotmail.com>
2022-02-12 22:39:03 +0100

    following a tutorial on Express. Learned about getting and sending info, URLS, routes views and public

--- js/script.js
+++ js/script.js
@@ -3,6 +3,26 @@ const express = require("express")
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs")
+app.use(logger)
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.use(express.static("public"))
+
+
+const userRouter = require("../routes/users")
+
+app.use("/users", userRouter)
+
+function logger(req, res, next) {
+    console.log(req.originalUrl)
+    next()
+}
+
+app.listen(port, () => console.log("listening on port" + port))
+
+
 // GET(how you get info), PUT(how you change info), POST(how you add info), DELETE(how you delete info)
 
 // app.get("/"= is the main path, (req(request), res(repsonse) = callback)
@@ -12,12 +32,17 @@ const port = 3000;
 //   res.send("Hello world")
 // })
 
-app.use(express.static("./"))
-
-app.listen(port, () => console.log("listening on port" + port))
+// dit om de html te laden
+// app.use(express.static("./"))
 
 // app.route("/about")
 // .get((req, res) =>{
 //   res.send("./about")
 // })
 // .post((req, res) =>{});
+
+// 404 error
+// res.status(404).send("404 not found")
+
+// helpful
+// https://www.youtube.com/watch?v=SccSCuHhOw0
