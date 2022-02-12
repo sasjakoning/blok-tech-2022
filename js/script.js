771aff98bf6c55993c2f52551e3b29a0e118e841
sasjakoning <sasjakoning@hotmail.com>
2022-02-12 18:46:24 +0100

    messing around with Express and routes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
-const nodemon = require("nodemon")
-const express = require('express')
-const app = express()
+const express = require("express")
 
-app.get('/', function (req, res) {
-  res.send('Hello wsadsawWorld')
-})
+const app = express();
+const port = 3000;
 
-app.listen(3000)
+// GET(how you get info), PUT(how you change info), POST(how you add info), DELETE(how you delete info)
+
+// app.get("/"= is the main path, (req(request), res(repsonse) = callback)
+
+// app.get("/", (req, res)=>{
+//   // send things back
+//   res.send("Hello world")
+// })
+
+app.use(express.static("./"))
+
+app.listen(port, () => console.log("listening on port" + port))
+
+// app.route("/about")
+// .get((req, res) =>{
+//   res.send("./about")
+// })
+// .post((req, res) =>{});
