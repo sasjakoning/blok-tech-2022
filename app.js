d1d91a6c8a9da5d734c38bfdd67cdfeafffd1744
sasja koning <sasjakoning@hotmail.com>
2022-02-17 15:42:39 +0100

    changed: contents of several hbs files and app.js.

--- app.js
+++ app.js
@@ -1,28 +1,56 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 const port = 3000;
 
-const handlebars = require("express-handlebars")
+const handlebars = require("express-handlebars");
 
-app.set("view engine", "hbs")
+app.set("view engine", "hbs");
 
-app.engine("hbs", handlebars.engine({
+app.engine(
+  "hbs",
+  handlebars.engine({
     layoutsDir: __dirname + "/views/layouts",
-    extname: 'hbs',
-    defaultLayout: 'planB',
-    partialsDir: __dirname + "/views/partials/"
-}))
+    extname: "hbs",
+    defaultLayout: "planB",
+    partialsDir: __dirname + "/views/partials/",
+  })
+);
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
+fakeApi = () => {
+  return [
+    {
+      name: "Katarina",
+      lane: "midlaner",
+    },
+    {
+      name: "Jayce",
+      lane: "toplaner",
+    },
+    {
+      name: "Heimerdinger",
+      lane: "toplaner",
+    },
+    {
+      name: "Zed",
+      lane: "midlaner",
+    },
+    {
+      name: "Azir",
+      lane: "midlaner",
+    },
+  ];
+};
 
 app.get("/", (req, res) => {
-    res.render('main', {layout: 'index'});
-})
-
+  res.render("main", { layout: "index", suggestedChamps: fakeApi (), listExists: true});
+});
 
 app.get("*", (req, res) => {
-    res.send(`${404} not found`)
-} )
+  res.send(`${404} not found`);
+});
 
-app.listen(port, () => console.log(`App listening to port ${port}`))
+app.listen(port, () => console.log(`App listening to port ${port}`));
+
+// useful: https://waelyasmina.medium.com/a-guide-into-using-handlebars-with-your-express-js-application-22b944443b65
