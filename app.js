4b4cf0a090b47cb37724a561fd384c0c9b697dd4
sasja koning <sasjakoning@hotmail.com>
2022-02-17 15:25:25 +0100

    removed: ejs. added: express-handlebars

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require("express")
 const app = express()
+const port = 3000;
+
+const handlebars = require("express-handlebars")
+
+app.set("view engine", "hbs")
+
+app.engine("hbs", handlebars.engine({
+    layoutsDir: __dirname + "/views/layouts",
+    extname: 'hbs',
+    defaultLayout: 'planB',
+    partialsDir: __dirname + "/views/partials/"
+}))
 
 app.use(express.static("public"))
 
+
 app.get("/", (req, res) => {
-    res.send("Hello world")
+    res.render('main', {layout: 'index'});
 })
 
-app.get("/about", (req, res) => {
-    res.send("About me")
-})
 
 app.get("*", (req, res) => {
     res.send(`${404} not found`)
 } )
 
-app.listen(3000)
+app.listen(port, () => console.log(`App listening to port ${port}`))
