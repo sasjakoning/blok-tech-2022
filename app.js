7bbba987e0481a7cd1e377898cdc1c0035915b58
sasja koning <sasjakoning@hotmail.com>
2022-03-03 15:28:51 +0100

    edited app.js

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+
 const handlebars = require("express-handlebars");
 const bodyParser = require("body-parser");
-const multer = require("multer");
+// const multer = require("multer");
 
 app.set("view engine", "hbs");
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.engine(
   "hbs",
@@ -22,56 +23,14 @@ app.engine(
 
 app.use(express.static("public"));
 
-users = () => {
-  return [
-    {
-      name: "Katarina",
-      realName: "Katy Johnson",
-    },
-    {
-      name: "Gollum",
-      realName: "Rick Smith",
-    },
-    {
-      name: "Deathbringer",
-      realName: "Eric Paul",
-    },
-    {
-      name: "Butterfy Lily",
-      realName: "Sarah Burger",
-    },
-    {
-      name: "Azir",
-      realName: "Micheal Beach",
-    },
-    {
-      name: "Azir",
-      realName: "Micheal Beach",
-    },
-    {
-      name: "Azir",
-      realName: "Micheal Beach",
-    },
-  ];
-};
+
 
 app.get("/", (req, res) => {
   res.render("main", {
-    layout: "index",
-    users: users(),
+    layout: "index"
   });
 });
 
-// test voor de buttons like dislike
-app.get("/register", (req, res) => {
-  const email = req.body.email;
-
-  res.send("worked");
-
-  const data = {
-    email,
-  };
-});
 
 app.get("*", (req, res) => {
   res.send(`${404} not found`);
