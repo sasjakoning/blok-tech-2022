01a68bbda2f8af83e51b525471ec1e3a006bbb9d
sasjakoning <sasjakoning@hotmail.com>
2022-03-18 21:48:52 +0100

    removed: old files

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const nodemon = require("nodemon")
-const express = require('express')
-const app = express()
-
-app.get('/', function (req, res) {
-  res.send('Hello wsadsawWorld')
-})
-
-app.listen(3000)
