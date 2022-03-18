b1b4853c0e15e99cf55483dc607f7abcb40e0770
sasja koning <sasjakoning@hotmail.com>
2022-03-18 14:13:39 +0100

    added: backside quotes

--- public/js/script.js
+++ public/js/script.js
@@ -8,56 +8,55 @@ window.addEventListener("load", () => {
 /* like dislike */
 /****************/
 
-// check if on correct page
-if (document.URL.includes("like")) {
-  const actionOverlay = document.querySelector(".actionOverlay");
-  const actionOverlayImg = actionOverlay.querySelector("img");
+const actionOverlay = document.querySelector(".actionOverlay");
+const actionOverlayImg = actionOverlay.querySelector("img");
 
-  const card = document.querySelector(".largeCard:first-of-type");
+const card = document.querySelector(".flipContainer:first-of-type");
 
-  const likeForm = document.querySelector(".form-like");
+const likeForm = document.querySelector(".form-like");
+console.log(likeForm);
+console.log("hello");
 
-  likeForm.addEventListener("submit", (e) => {
-    console.log("submitted");
+likeForm.addEventListener("submit", (e) => {
+  console.log("submitted");
 
-    e.preventDefault();
+  e.preventDefault();
 
-    card.classList.add("cardLike");
-    actionOverlay.classList.add("actionLiked");
-    actionOverlayImg.src = "/images/overlayLike.svg";
+  card.classList.add("cardLike");
+  actionOverlay.classList.add("actionLiked");
+  actionOverlayImg.src = "/images/overlayLike.svg";
 
-    card.addEventListener("animationend", () => {
-      likeForm.submit();
-    });
+  card.addEventListener("animationend", () => {
+    likeForm.submit();
   });
+});
 
-  const dislikeForm = document.querySelector(".form-dislike");
+const dislikeForm = document.querySelector(".form-dislike");
 
-  dislikeForm.addEventListener("submit", (e) => {
-    console.log("submitted");
+dislikeForm.addEventListener("submit", (e) => {
+  console.log("submitted");
 
+  e.preventDefault();
+
+  card.classList.add("cardDislike");
+  actionOverlay.classList.add("actionDisliked");
+  actionOverlayImg.src = "/images/overlayDislike.svg";
+
+  setTimeout(() => {
+    dislikeForm.submit();
+  }, 500);
+});
+
+// match popup
+const continueBtn = document.querySelector(".continue");
+const matchBackground = document.querySelector(".matchBackground");
+const matchPopup = document.querySelector(".matchPopup");
+
+//check if element exists
+if (matchBackground != null) {
+  continueBtn.addEventListener("click", (e) => {
     e.preventDefault();
-
-    card.classList.add("cardDislike");
-    actionOverlay.classList.add("actionDisliked");
-    actionOverlayImg.src = "/images/overlayDislike.svg";
-
-    setTimeout(() => {
-      dislikeForm.submit();
-    }, 500);
+    matchBackground.remove();
+    matchPopup.remove();
   });
-
-  // match popup
-  const continueBtn = document.querySelector(".continue");
-  const matchBackground = document.querySelector(".matchBackground");
-  const matchPopup = document.querySelector(".matchPopup");
-
-  //check if element exists
-  if (matchBackground != null) {
-    continueBtn.addEventListener("click", (e) => {
-      e.preventDefault();
-      matchBackground.remove();
-      matchPopup.remove();
-    });
-  }
 }
